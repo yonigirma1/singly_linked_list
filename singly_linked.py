1168d073ca25b702f33732bec74c782e2e98d810
Yonatan Girma <ygirma.exclusive@yahoo.com>
2018-01-14 19:03:31 -0600

    Singly linked list

---
+++ singly_linked.py
@@ -0,0 +1,14 @@
+#How to create a singly linked list
+
+class Node(self,value) :
+    self.value = value
+    self.nextNode = None
+
+# a = Node(1)
+# b = Node(2)
+# c = Node(3)
+
+# a.nextNode = b
+# b.nextNode = c
+
+                   
